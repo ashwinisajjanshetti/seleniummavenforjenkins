0ff0b54f0abe07de43e509d460f9b8f05b0532d2
username <ashwinisajjanshetti@gmail.com>
2025-05-09 13:43:33 +0530

    parameter added

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -2,17 +2,32 @@ import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
 
 public class UITest {
 
+	@Parameters("Browser")
 	@Test
-	public void startbrowser()
+	public void startbrowser(String browserName)
 	{
-		WebDriver driver=new ChromeDriver();
+		System.out.println("parameter value is"+browserName);
+		WebDriver driver=null;
+		
+		if(browserName.equalsIgnoreCase("Chrome"))
+		{
+			 
+			driver=new ChromeDriver();
+		}
+		else if(browserName.equalsIgnoreCase("Edge"))
+		{
+			 driver=new EdgeDriver();
+		}
+		
 		driver.get("https://www.orangehrm.com/");
 		Assert.assertTrue(driver.getTitle().contains("Orange"),"Title does not match");
 		driver.close();
